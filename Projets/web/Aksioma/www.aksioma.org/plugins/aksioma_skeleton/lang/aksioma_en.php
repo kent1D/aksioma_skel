b3b06df77839f421284bf13f2826a554ade8ca97
kent1 <kent1@6150db3a-0392-43c0-ab3a-2e9def4689b5>
2010-12-11 20:49:36 +0000

    Un début de configuration du squelette

--- Projets/web/Aksioma/www.aksioma.org/plugins/aksioma_skeleton/lang/aksioma_en.php
+++ Projets/web/Aksioma/www.aksioma.org/plugins/aksioma_skeleton/lang/aksioma_en.php
@@ -1,12 +1,14 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/contact/lang/
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
 	'cfg_titre' => 'Aksioma setup',
+
+	// L
+	'label_contenu_colonne_3' => 'Sections in the third column',
 	
 );
 
