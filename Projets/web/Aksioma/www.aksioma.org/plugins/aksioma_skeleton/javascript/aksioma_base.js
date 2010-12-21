14136cc993784ae37179b7c8b3a69b20519af7cf
kent1 <kent1@6150db3a-0392-43c0-ab3a-2e9def4689b5>
2010-12-21 16:50:44 +0000

    Grosse amélioration de la page agenda
    Amélioration du portfolio sur les projets

--- Projets/web/Aksioma/www.aksioma.org/plugins/aksioma_skeleton/javascript/aksioma_base.js
+++ Projets/web/Aksioma/www.aksioma.org/plugins/aksioma_skeleton/javascript/aksioma_base.js
@@ -1,17 +1,42 @@
 $(function() {
-	var i = 0;
-	$('.extra-right ul.liste').each(function(){
-		i++;
-		var text = '<a id="prev'+i+'" class="prev" href="#">&lt;</a>';
-		text += '<a id="next'+i+'" class="next" href="#">&gt;</a>';
-		$(this).after(text)
-		$(this).carouFredSel({
-			auto:false,
-			items: 3,
-			width: 276,
-			prev: "#prev"+i,
-			next: "#next"+i
+	var carousel = function(){
+		var i = 0;
+		$('.extra-right').find('.prev,.next').detach();
+		$('.extra-right ul.liste').each(function(){
+			i++;
+			if($(this).parent().is('.caroufredsel_wrapper')){
+				$(this).unwrap();
+			}
+			var text = '<a id="prev'+i+'" class="prev" href="#">&lt;</a>';
+			text += '<a id="next'+i+'" class="next" href="#">&gt;</a>';
+			$(this).after(text);
+			$(this).carouFredSel({
+				auto:false,
+				items: 3,
+				width: 276,
+				prev: "#prev"+i,
+				next: "#next"+i
+			});
 		});
-	});
+		var j = 0;
+		$('.documents_portfolio ul.liste').each(function(){
+			j++;
+			if($(this).parent().is('.caroufredsel_wrapper')){
+				$(this).unwrap();
+			}
+			var text = '<a id="prev'+j+'" class="prev" href="#">&lt;</a>';
+			text += '<a id="next'+j+'" class="next" href="#">&gt;</a>';
+			$(this).after(text);
+			carousel_port = $(this).carouFredSel({
+				auto:false,
+				items: 3,
+				width: 346,
+				prev: "#prev"+j,
+				next: "#next"+j
+			});
+		});
+	}
+	carousel();
+	onAjaxLoad(carousel);
 	$('a.spip_out').attr('target','_blank');
 });
