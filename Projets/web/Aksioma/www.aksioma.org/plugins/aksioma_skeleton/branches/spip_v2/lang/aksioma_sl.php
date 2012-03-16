9c4ebf24c91d6adcbf0b5bcd4d235608e8cdb38b
kent1 <kent1@6150db3a-0392-43c0-ab3a-2e9def4689b5>
2012-03-16 19:24:21 +0000

    la branche pour SPIP v2

---
+++ Projets/web/Aksioma/www.aksioma.org/plugins/aksioma_skeleton/branches/spip_v2/lang/aksioma_sl.php
@@ -0,0 +1,61 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_titre' => 'Aksioma setup',
+
+	// E
+	'evenement_voir' => 'Glej',
+
+	// I
+	'info_back' => 'BACK',
+	'info_home' => 'HOME',
+	'info_more' => 'MORE',
+	'info_site_auteur' => 'Website',
+	'info_sites' => 'MORE',
+
+	// L
+	'label_background_color' => 'Background color of the content area',
+	'label_banner_luhan' => 'Article for the images of the Mac Luhan banner',
+	'label_body_color' => 'Background color of the page',
+	'label_bold_color' => 'Bold color',
+	'label_composition_projet' => 'Default composition for a project',
+	'label_contenu_colonne_1' => 'Sections in the first column',
+	'label_contenu_colonne_3' => 'Sections in the third column',
+	'label_contenu_colonne_4' => 'Sections in the fourth column',
+	'label_contenu_colonne_medias' => 'Root section of the media content',
+	'label_link_color' => 'Color of the links',
+	'label_link_hover_color' => 'Color of the links (hovering)',
+	'label_rubrique_projet' => 'Projects section',
+	'label_rubrique_project_space' => 'Project Space section',
+	'label_text_color' => 'Color of the text',
+	'label_texte_defaut_projet' => 'Default text of a project section',
+	'label_title_color' => 'Title color',
+	'label_titre_contenu_colonne_4' => 'Title of the content in the fourth column',
+	'label_titre_contenu_colonne_medias' => 'Title of the media bloc',
+	'lien_abonnement_listes' => 'newsletter [Sign up]',
+	'lien_today' => 'Danes',
+
+	// M
+	'message_champ_search' => 'vpiši iskani niz',
+	
+	// T
+	'titre_about_auteur' => 'About the author',
+	'titre_about_auteurs' => 'About the authors',
+	'titre_articles_auteur' => 'Other articles from this author',
+	'titre_articles_lies' => 'Povezani članki',
+	'titre_events_ongoing' => 'Aktualni dogodki',
+	'titre_events_next' => 'Prihajajoči dogodki',
+	'titre_last_events' => 'Nedavni dogodki',
+	'titre_medias' => 'MEDIAS',
+	'titre_portfolio' => 'PORTFOLIO',
+	'titre_press_corner' => 'ZA MEDIJE',
+	'titre_publications' => 'PUBLICATIONS',
+	'titre_related_events' => 'POVEZANI DOGODKI',
+	
+);
+
+?>
