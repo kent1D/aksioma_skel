af6e1cc241d5a7e19b6faeff7d86594e63d99912
kent1 <kent1@arscenic.info>
2012-03-16 19:17:52 +0000

    On a un trunk

---
+++ aksioma_options.php
@@ -0,0 +1,13 @@
+<?php
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$forcer_lang = true;
+
+$debut_date_publication = 0;
+
+$table_des_traitements['TITRE'][]= 'typo(supprimer_numero(%s))';
+$table_des_traitements['SOUSTITRE'][]= 'typo(%s)';
+
+$quota_cache = 100;
+
+?>
