65714a0f39214ec461ad39628a2ef524b7731444
kent1 <kent1@6150db3a-0392-43c0-ab3a-2e9def4689b5>
2012-03-21 18:29:14 +0000

    On passe au paquet.xml

---
+++ Projets/web/Aksioma/www.aksioma.org/plugins/aksioma_skeleton/trunk/lang/paquet-aksioma_en.php
@@ -0,0 +1,20 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-aksioma
+// Langue: en
+// Date: 21-03-2012 17:38:07
+// Items: 3
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// A
+	'aksioma_description' => 'Skeleton for the website [Aksioma.org->http://www.aksioma.org]',
+	'aksioma_nom' => 'Aksioma skeleton',
+	'aksioma_slogan' => 'Skeleton for the website Aksioma',
+);
+?>
---
+++ Projets/web/Aksioma/www.aksioma.org/plugins/aksioma_skeleton/trunk/lang/paquet-aksioma_fr.php
@@ -0,0 +1,20 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-aksioma
+// Langue: fr
+// Date: 21-03-2012 17:38:07
+// Items: 3
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// A
+	'aksioma_description' => 'Squelette pour le site [Aksioma.org->http://www.aksioma.org]',
+	'aksioma_nom' => 'Squelette Aksioma',
+	'aksioma_slogan' => 'Squelette pour le site Aksioma',
+);
+?>
