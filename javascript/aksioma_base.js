3054fbe60aaf24a7e0a53869bbe8ddd6a9cf2cdc
kent1 <kent1@arscenic.info>
2012-03-21 15:46:39 +0000

    Trop de trucs à raconter, on va juste dire qu'on avance bien

--- javascript/aksioma_base.js
+++ javascript/aksioma_base.js
@@ -6,18 +6,28 @@ $(function() {
 			i++;
 			if($(this).parent().is('.caroufredsel_wrapper')){
 				$(this).unwrap();
+				$('#prev'+i+',#next'+i).detach();
 			}
 			var text = '<a id="prev'+i+'" class="prev" href="#">&lt;</a>';
 			text += '<a id="next'+i+'" class="next" href="#">&gt;</a>';
 			$(this).after(text);
-			$(this).find('li').equalHeights();
+			$(this).find('li').height('auto').equalHeights();
+			var size = $(this).parent().width()-($('#prev'+i).outerWidth())-(13*3);
+			$(this).find('li').width((size -($('#next'+i).outerWidth()))/3);
+			size = size - (7*3);
+			var prev = $(this).parent().find('.prev');
+			var next = $(this).parent().find('.next');
 			$(this).carouFredSel({
 				auto:false,
 				items: 3,
-				width: 276,
+				width:size,
 				prev: "#prev"+i,
 				next: "#next"+i
 			});
+			$(this).find('li img.spip_logos').css({'width':'100%','height':'auto'});
+			var height = ($(this).find('li').eq(0).outerHeight());
+			prev.css('top',(height/2)-(prev.height()/2));
+			next.css('top',(height/2)-(next.height()/2));
 		});
 		var j = 0;
 		$('.documents_portfolio ul.liste').each(function(){
@@ -42,18 +52,29 @@ $(function() {
 			k++;
 			if($(this).parent().is('.caroufredsel_wrapper')){
 				$(this).unwrap();
+				$('#prev'+k+',#next'+k).detach();
 			}
 			var text = '<a id="prev'+k+'" class="prev" href="#">&lt;</a>';
 			text += '<a id="next'+k+'" class="next" href="#">&gt;</a>';
 			$(this).after(text);
+			$(this).find('li').height('auto');
 			$(this).find('li').equalHeights();
+			var size = $(this).parent().width()-($('#prev'+k).outerWidth())-(12*5);
+			size = size - (7*3);
+			var prev = $(this).parent().find('.prev');
+			var next = $(this).parent().find('.next');
+			$(this).find('li').width((size-($('#next'+k).outerWidth()))/5);
+			var height = ($(this).find('li').eq(0).outerHeight());
+			prev.css('top',(height/2)-(prev.height()/2));
+			next.css('top',(height/2)-(next.height()/2));
 			carousel_port = $(this).carouFredSel({
 				auto:false,
-				items: 5,
-				width: 940,
+				width:size,
 				prev: "#prev"+k,
 				next: "#next"+k
 			});
+			$(this).find('li img.spip_logos').css({'width':'100%','height':'auto'});
+			//$(this).find('li img.spip_logos').css({'width':'100%','height':'auto'});
 		});
 		var l = 0;
 		if($('.page_rubrique #contenu ul.liste_images li').size() > 0){
@@ -93,7 +114,7 @@ $(function() {
 	}
 	
 	var project_same_height = function(){
-		$('.rubrique_projet_normal #navigation,.rubrique_projet_normal #contenu').equalHeights();
+		$('.rubrique_projet_normal #navigation,.rubrique_projet_normal #contenu').height('auto').equalHeights();
 	}
 	$(document).ready(function() {
 		$('a[rel="external"],a.spip_out').click(function(){
@@ -105,4 +126,8 @@ $(function() {
 	search_placeholder();
 	onAjaxLoad(carousel);
 	onAjaxLoad(project_same_height);
+	$(window).resize(function(){
+		project_same_height();
+		carousel();
+	});
 });
