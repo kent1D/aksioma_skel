e3afc1580feab287232f5045e049b74ed16620e7
kent1 <kent1@6150db3a-0392-43c0-ab3a-2e9def4689b5>
2012-05-18 13:07:50 +0000

    On commence à traduire SPIP en sl donc on s'en sert

---
+++ Projets/web/Aksioma/www.aksioma.org/plugins/aksioma_skeleton/trunk/lang/agenda_sl.php
@@ -0,0 +1,147 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// A
+'absence_prise_en_compte' => '<NEW>Votre absence est enregistrée',
+'activite_editoriale' => '<NEW>Activité Éditoriale',
+'afficher_calendrier' => '<NEW>Afficher le calendrier',
+'agenda' => '<NEW>Agenda',
+'ajouter_mots_cles' => '<NEW>Ajouter des mots-clés',
+'ajouter_repetition' => '<NEW>Ajouter des répétitions',
+'ajouter_un_evenement' => '<NEW>ajouter un événement à cet article',
+'annee_precedente' => '<NEW>année précédente',
+'annee_suivante' => '<NEW>année suivante',
+'aucun_evenement' => '<NEW>aucun événement',
+'aucune_rubrique_mode_agenda' => '<NEW>Par défaut, toutes les rubriques permettent d\'utiliser les événements. Si vous activez le mode agenda sur une ou plusieurs rubriques, la gestion des événements sera limitée a sa branche.',
+
+// B
+'bouton_annuler' => '<NEW>Annuler',
+'bouton_supprimer' => '<NEW>Supprimer',
+
+// C
+'confirm_suppression_inscription' => '<NEW>Voulez-vous vraiment supprimer cette inscription ?',
+'connexion_necessaire_pour_inscription' => '<NEW>Merci de vous connecter pour pouvoir vous inscrire à cet événement.',
+'creer_evenement' => '<NEW>Créer un événement',
+
+// E
+'erreur_article_interdit' => '<NEW>Vous n\'avez pas le droit d\'associer cet événement à cet article',
+'erreur_article_manquant' => '<NEW>Vous devez indiquer un article',
+'erreur_date' => '<NEW>Cette date est incorrecte',
+'erreur_date_avant_apres' => '<NEW>Indiquez une date de fin après la date de début !',
+'erreur_date_corrigee' => '<NEW>La date a été corrigée',
+'erreur_heure' => '<NEW>Cette heure est incorrecte',
+'erreur_heure_corrigee' => '<NEW>L\'heure a été corrigée',
+'evenement_adresse' => '<NEW>Adresse',
+'evenement_article' => '<NEW>Associé à l\'article',
+'evenement_autres_occurences' => '<NEW>Autres occurences :',
+'evenement_date' => 'Datum',
+'evenement_date_a' => '<NEW>à ',
+'evenement_date_a_immediat' => '<NEW>à ',
+'evenement_date_au' => 'Do ',
+'evenement_date_de' => '<NEW>De ',
+'evenement_date_debut' => '<NEW>Date de début',
+'evenement_date_du' => 'Od ',
+'evenement_date_fin' => '<NEW>Date de fin',
+'evenement_descriptif' => '<NEW>Descriptif',
+'evenement_horaire' => '<NEW>Toute la journée',
+'evenement_lieu' => '<NEW>Lieu',
+'evenement_repetitions' => '<NEW>Répétitions',
+'evenement_titre' => '<NEW>Titre',
+'evenements' => 'Dogodki',
+'evenements_a_venir' => '<NEW>À venir',
+'evenements_depuis_debut' => '<NEW>Depuis le début',
+
+// F
+'fermer' => '<NEW>fermer',
+
+// G
+'groupes_mots' => '<NEW>Groupes de mots-clé',
+
+// I
+'icone_creer_evenement' => '<NEW>Créer un nouvel événement',
+'icone_modifier_evenement' => '<NEW>Modifier l\'événement',
+'indiquez_votre_choix' => '<NEW>Indiquez votre choix',
+'info_1_place' => '<NEW>1 place',
+'info_aucun_evenement' => '<NEW>Aucun événement',
+'info_evenement' => 'Dogodek',
+'info_evenement_poubelle' => '<NEW>Événement supprimé',
+'info_evenement_propose' => '<NEW>Événement proposé',
+'info_evenement_publie' => '<NEW>Événement publié',
+'info_evenements' => 'Dogodki',
+'info_inscription' => '<NEW>Inscription en ligne :',
+'info_lieu' => '<NEW>Lieu :',
+'info_nb_places' => '<NEW>@nb@ places',
+'info_nombre_evenements' => '@nb@ dogodki',
+'info_nouvel_evenement' => 'Nov dogodek',
+'info_reponse_inscription_non' => 'ne',
+'info_reponse_inscription_nsp' => '?',
+'info_reponse_inscription_oui' => 'ja',
+'info_reponses_inscriptions' => '<NEW>Réponses :',
+'info_un_evenement' => '1 dogodek',
+'inscrits' => '<NEW>Inscriptions',
+'item_mots_cles_association_evenements' => '<NEW>aux événements',
+
+// L
+'label_inscription' => '<NEW>Inscription en ligne',
+'label_places' => '<NEW>Limiter le nombre de places',
+'label_reponse_jyparticipe' => '<NEW>J\'y serai',
+'label_reponse_jyparticipe_pas' => '<NEW>Je n\'y vais pas',
+'label_reponse_jyparticipe_peutetre' => '<NEW>J\'y serai peut-être',
+'label_vous_inscrire' => '<NEW>Votre participation',
+'lien_desinscrire' => '<NEW>Supprimer',
+'lien_retirer_evenement' => '<NEW>Supprimer',
+'liste_inscrits' => '<NEW>Liste des inscriptions',
+
+// M
+'mois_precedent' => 'prejšnji mesec',
+'mois_suivant' => '<NEW>mois suivant',
+
+// N
+'nb_mots_clefs' => '<NEW>@nb@ mots-clés',
+'nb_repetitions' => '<NEW>@nb@ répétitions',
+
+// P
+'participation_incertaine_prise_en_compte' => '<NEW>Votre participation éventuelle est enregistrée',
+'participation_prise_en_compte' => '<NEW>Votre participation est enregistrée',
+'probleme_technique' => '<NEW>Un problème technique est survenu. Reessayez plus tard.',
+
+// R
+'repetition' => '<NEW>Répétition',
+'repetition_de' => '<NEW>Répétition de',
+'rubrique_activer_agenda' => '<NEW>Activer le mode agenda',
+'rubrique_dans_une_rubrique_mode_agenda' => '<NEW>Cette rubrique permet d\'utiliser les événements car elle est dans une rubrique dont le mode agenda a été activé',
+'rubrique_desactiver_agenda' => '<NEW>Désactiver le mode agenda',
+'rubrique_liste_evenements_de' => '<NEW>Événements de la rubrique',
+'rubrique_mode_agenda' => '<NEW>Le mode agenda est activé pour cette rubrique et sa branche',
+'rubrique_sans_gestion_evenement' => '<NEW>Le mode agenda n\'est pas activé pour cette rubrique',
+'rubriques' => '<NEW>Rubriques Agenda',
+
+// S
+'sans_titre' => '<NEW>(sans titre)',
+
+// T
+'telecharger' => '<NEW>Télécharger',
+'texte_agenda' => '<NEW>AGENDA',
+'texte_evenement_statut' => '<NEW>Cet événement est :',
+'texte_logo_objet' => '<NEW>LOGO DE L\'EVENEMENT',
+'titre_cadre_ajouter_evenement' => '<NEW>Ajouter un événement',
+'titre_cadre_modifier_evenement' => '<NEW>Modifier un événement',
+'titre_sur_l_agenda' => '<NEW>Sur l\'agenda',
+'toutes_rubriques' => '<NEW>Toutes',
+
+// U
+'un_mot_clef' => '<NEW>1 mot-clé',
+'une_repetition' => '<NEW>1 répétition',
+
+// V
+'voir_evenements_rubrique' => '<NEW>Voir les événements de la rubrique',
+
+// Z
+'zz_timestamp_nepastraduire' => '2012-05-15 12:36:06'
+
+);
+
+?>
